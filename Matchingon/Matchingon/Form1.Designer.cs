222cde0c9e5f4567051d081b66e297caa714fe50
Abdullayews <abdullayev6006@gmail.com>
2026-05-02 20:13:22 +0400

    Bütün qovluq sinxronlaşdırıldı

---
+++ Matchingon/Matchingon/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace Matchingon
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.scorePanel = new System.Windows.Forms.Panel();
+            this.btnRestart = new System.Windows.Forms.Button();
+            this.lblTimer = new System.Windows.Forms.Label();
+            this.lblAttempts = new System.Windows.Forms.Label();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.timerUpdater = new System.Windows.Forms.Timer(this.components);
+            this.scorePanel.SuspendLayout();
+            this.SuspendLayout();
+
+            // scorePanel
+            this.scorePanel.BackColor = System.Drawing.Color.FromArgb(44, 62, 80);
+            this.scorePanel.Controls.Add(this.btnRestart);
+            this.scorePanel.Controls.Add(this.lblTimer);
+            this.scorePanel.Controls.Add(this.lblAttempts);
+            this.scorePanel.Controls.Add(this.lblScore);
+            this.scorePanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.scorePanel.Location = new System.Drawing.Point(0, 0);
+            this.scorePanel.Name = "scorePanel";
+            this.scorePanel.Padding = new System.Windows.Forms.Padding(15);
+            this.scorePanel.Size = new System.Drawing.Size(900, 70);
+            this.scorePanel.TabIndex = 0;
+
+            // btnRestart
+            this.btnRestart.BackColor = System.Drawing.Color.FromArgb(52, 152, 219);
+            this.btnRestart.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnRestart.FlatAppearance.BorderSize = 0;
+            this.btnRestart.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRestart.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.btnRestart.ForeColor = System.Drawing.Color.White;
+            this.btnRestart.Location = new System.Drawing.Point(650, 16);
+            this.btnRestart.Name = "btnRestart";
+            this.btnRestart.Size = new System.Drawing.Size(170, 38);
+            this.btnRestart.TabIndex = 3;
+            this.btnRestart.Text = "🔄 Yenidən Başla";
+            this.btnRestart.UseVisualStyleBackColor = false;
+            this.btnRestart.Click += new System.EventHandler(this.BtnRestart_Click);
+
+            // lblTimer
+            this.lblTimer.AutoSize = true;
+            this.lblTimer.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.lblTimer.ForeColor = System.Drawing.Color.FromArgb(231, 76, 60);
+            this.lblTimer.Location = new System.Drawing.Point(480, 22);
+            this.lblTimer.Name = "lblTimer";
+            this.lblTimer.Size = new System.Drawing.Size(78, 21);
+            this.lblTimer.TabIndex = 2;
+            this.lblTimer.Text = "⏱️ 00:00";
+
+            // lblAttempts
+            this.lblAttempts.AutoSize = true;
+            this.lblAttempts.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.lblAttempts.ForeColor = System.Drawing.Color.FromArgb(241, 196, 15);
+            this.lblAttempts.Location = new System.Drawing.Point(280, 22);
+            this.lblAttempts.Name = "lblAttempts";
+            this.lblAttempts.Size = new System.Drawing.Size(110, 21);
+            this.lblAttempts.TabIndex = 1;
+            this.lblAttempts.Text = "🎯 Cəhdlər: 0";
+
+            // lblScore
+            this.lblScore.AutoSize = true;
+            this.lblScore.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.lblScore.ForeColor = System.Drawing.Color.FromArgb(46, 204, 113);
+            this.lblScore.Location = new System.Drawing.Point(25, 22);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(126, 21);
+            this.lblScore.TabIndex = 0;
+            this.lblScore.Text = "⭐ Cütlər: 0 / 32";
+
+            // tableLayoutPanel1
+            this.tableLayoutPanel1.BackColor = System.Drawing.Color.FromArgb(45, 52, 54);
+            this.tableLayoutPanel1.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.tableLayoutPanel1.ColumnCount = 10;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 5F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.875F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.875F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.875F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.875F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.875F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.875F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.875F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.875F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 5F));
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 70);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 10;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.875F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.875F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.875F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.875F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.875F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.875F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.875F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.875F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(900, 750);
+            this.tableLayoutPanel1.TabIndex = 1;
+
+            // timerUpdater
+            this.timerUpdater.Interval = 1000;
+            this.timerUpdater.Tick += new System.EventHandler(this.TimerUpdater_Tick);
+
+            // Form1
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(30, 30, 30);
+            this.ClientSize = new System.Drawing.Size(900, 820);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.scorePanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Matchingon";
+            this.scorePanel.ResumeLayout(false);
+            this.scorePanel.PerformLayout();
+            this.ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel scorePanel;
+        private System.Windows.Forms.Button btnRestart;
+        private System.Windows.Forms.Label lblTimer;
+        private System.Windows.Forms.Label lblAttempts;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Timer timerUpdater;
+    }
+}
